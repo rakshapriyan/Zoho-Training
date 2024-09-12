36b01782464602e55f885c8b7898eacbb8e03dca
rakshapriyan <rakshapriyanraksha@gmail.com>
2024-09-12 10:19:04 +0530

    Exercise 4

---
+++ Exercise4/Parrot.java
@@ -0,0 +1,7 @@
+package child;
+import bird.Bird;
+public class Parrot extends Bird{
+	public void speak(){
+		System.out.println("Am Speaking");
+	}
+}
---
+++ Exercise4/TestRunner.java
@@ -0,0 +1,12 @@
+package test;
+import bird.Bird;
+import child.Parrot;
+
+public class TestRunner{
+	public static void main(String[] args){
+		Parrot parrot = new Parrot();
+		parrot.fly();
+		parrot.speak();
+
+	}
+}
